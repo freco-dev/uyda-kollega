6427ad3440e260275634990284fa770a5b7923bf
freco-dev <computer.studio77@gmail.com>
2025-12-12 17:20:35 +0500

    ngrok

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const apiLink = "https://6539527d4603.ngrok-free.app/api/Golddishes/hs/kollega";
+const apiLink = "https://007807f91edc.ngrok-free.app/api/Golddishes/hs/kollega";
 
 export default apiLink;
