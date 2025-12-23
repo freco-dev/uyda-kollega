5644e8be21b1c919580b7db5e621d51540226324
freco-dev <computer.studio77@gmail.com>
2025-12-23 23:50:30 +0500

    Revert "uotade fuckin ngrok link"
    This reverts commit 3aa0f5eeb9d1a516c528dd80a15ac03f08a40a2b.

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 const apiLink = "https://corliss-unmetaphysic-evaluatingly.ngrok-free.dev/api/Golddishes/hs/kollega";
+=======
+const apiLink = "https://api.erkaboyev.uz/Golddishes/hs/kollega";
+>>>>>>> parent of 3aa0f5e (uotade fuckin ngrok link)
 
 export default apiLink;
--- src/i18n.js
+++ src/i18n.js
@@ -7,7 +7,7 @@ import apiLink  from "@/config/api"
 //User APIdan kelgan tilni olish
 const fetchLang= async () => {
   try {
-    const response = await axios.get(`${apiLink}/user?chatId=${Telegram.WebApp.initDataUnsafe.user}`, {
+    const response = await axios.get(`${apiLink}/user?chatId=${window.Telegram.WebApp.initDataUnsafe.user.id}`, {
       headers: {
         "Authorization": "Basic " + btoa("admin:57325732"),
         "Content-Type": "application/json",
@@ -15,13 +15,12 @@ const fetchLang= async () => {
       }
     });
     const language = response.data.data.language;
-    return language;  
+    return language;
   } catch (error) {
     console.error("Error fetching user language:", error);
     // Agar xato bo'lsa, default tilni o'rnatish
     const language = "uzbek-crylic"; // Default til
     return language;
-    
   }
 }
 
@@ -32,9 +31,17 @@ const messages = {
 
 const i18n = createI18n({
   legacy: false, // Composition API bilan ishlash uchun
-  locale: fetchLang(), // Default til
+  locale: 'uzbek-crylic', // Default til
   fallbackLocale: "russian", // Agar til topilmasa
   messages,
 });
+
+if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
+    fetchLang().then(lang => {
+        if (lang) {
+            i18n.global.locale.value = lang;
+        }
+    });
+}
 
 export default i18n;
